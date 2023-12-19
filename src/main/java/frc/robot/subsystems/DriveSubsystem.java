0396a3109b48634b1ab46618c433b6d170a3e79a
Craig Schardt <crschardt@fastem.com>
2023-12-19 02:17:54 -0600

    Applied spotless

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -8,7 +8,6 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
@@ -121,11 +120,7 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   public void resetDirection() {
-    resetOdometry(
-      new Pose2d(
-        getPose().getTranslation(), new Rotation2d(0.0)
-      )
-    );
+    resetOdometry(new Pose2d(getPose().getTranslation(), new Rotation2d(0.0)));
   }
 
   /**
